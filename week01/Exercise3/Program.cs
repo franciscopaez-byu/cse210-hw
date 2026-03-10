bd2c10a40da81e84c5656d1269a453fdcf3b3f59
franciscopaez-byu <fpaezromero@byupathway.edu>
2026-03-09 21:51:05 -0300

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomgenerator = new Random();
+        int magicnumber = randomgenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if(magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine("You guess it!");
+            }
+        }
+
+
+    
+    
     }
 }
